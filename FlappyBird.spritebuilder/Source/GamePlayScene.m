b11c256cf800425132a0407625c21b229e15ae56
Shriya Nevatia <sneva831@gmail.com>
2015-06-20 12:56:49 -0400

    Week 1 starting

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,11 +9,15 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    // this will be run every frame.
+    // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    // put the update code here
 }
 
 // put new methods here
